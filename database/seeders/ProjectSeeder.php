0d9612e45299c2444fadb0a53cc234d1066a1cc8
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-10-02 18:24:53 +0200

    Funziona il seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Schema;
 
 use App\Models\Project;
 use App\Models\Type;
-use App\Models\Technology;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectSeeder extends Seeder
 {
@@ -25,11 +25,35 @@ class ProjectSeeder extends Seeder
             Project::truncate();
         });
 
+        storage::deleteDirectory('uploads/images/');
+        storage::makeDirectory('uploads/images/');
+
         for($i=0;$i<20;$i++){
 
             $randomTypeId = null;
+            $img=null;
+
             if (fake()->boolean(75)) {
                 $randomTypeId = Type::inRandomOrder()->first()->id;
+                // $img = fake()->image(storage_path('app/public/uploads/images'), 360, 360, 'animals', false, true, null, false);
+             
+
+                    $imgPath = fake()->imageUrl();              
+                    $imgContent = file_get_contents($imgPath);              
+                    $newImagePath = storage_path('app/public/uploads/images');             
+                    $newImageName = rand(1000, 9999).'-'.rand(1000, 9999).'-'.rand(1000, 9999).'.png';             
+                    $fullNewImagePath = $newImagePath.'/'.$newImageName;              
+                    file_put_contents($fullNewImagePath, $imgContent);     
+
+                if($img!=''){
+                    $img="uploads/images/$newImageName";
+                    
+                }
+
+                else{
+                    $img=null;
+                    
+                }
             }
 
             $title=fake()->words(rand(1,3),true);
@@ -40,6 +64,7 @@ class ProjectSeeder extends Seeder
                 'slug'=>str()->slug($title),
                 'description'=>fake()->paragraph(2),
                 'type_id'=>$randomTypeId,
+                'img'=>$img,
             ]);
         };
     }
